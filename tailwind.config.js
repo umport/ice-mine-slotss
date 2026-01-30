30bfb71ef6c610a80203f7e7ecff560a76d622d8
umport <tk54vptpwy@privaterelay.appleid.com>
2026-01-30 07:24:44 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'float': 'float 6s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-20px)' },
+        }
+      },
+    },
+  },
+  plugins: [],
+}
