cafb6d3412b93461c134f56418d69af3fceb52cb
umport <tk54vptpwy@privaterelay.appleid.com>
2026-01-30 09:28:53 -0300

    Create _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#0c4a6e" />
+        <meta name="description" content="Ice Mine Slots - free social slot game" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
